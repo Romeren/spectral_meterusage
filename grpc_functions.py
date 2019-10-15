fb058fbbeec1a04fd8446bcf3dc4dadebc606a78
Emil $. Rømer <emilromer@hotmail.com>
2019-10-15 17:19:22 +0200

    Update grpc_functions.py

--- grpc_functions.py
+++ grpc_functions.py
@@ -1,5 +1,6 @@
 import csv
 from datetime import datetime, timedelta
+from filelock import FileLock
 
 def get_data(date_start, date_end):
     """
@@ -16,22 +17,23 @@ def get_data(date_start, date_end):
     # initialize return dictionary
     usage_dict = {}
     # open csv file
-    with open(filename, 'r') as meterusage_file:
-        meterusage_reader = csv.reader(meterusage_file)
-        # skip header row
-        next(meterusage_reader)
-        # loop through all rows
-        for row in meterusage_reader:
-            datetime_obj = datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S")
-            # if datetime falls within range
-            if datetime_obj >= start and datetime_obj <= end:
-                date = datetime_obj.date().strftime("%Y-%m-%d")
-                time = datetime_obj.time().strftime("%H:%M:%S")
-                # add to dictionary
-                if date in usage_dict:
-                    usage_dict[date].append({'time': time, 'value': row[1]})
-                else:
-                    usage_dict[date] = [{'time': time, 'value': row[1]}]
+    with FileLock(filename): # secure against multiple threads reading from the same file at the same time
+        with open(filename, 'r') as meterusage_file:
+            meterusage_reader = csv.reader(meterusage_file)
+            # skip header row
+            next(meterusage_reader)
+            # loop through all rows
+            for row in meterusage_reader:
+                datetime_obj = datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S")
+                # if datetime falls within range
+                if datetime_obj >= start and datetime_obj <= end:
+                    date = datetime_obj.date().strftime("%Y-%m-%d")
+                    time = datetime_obj.time().strftime("%H:%M:%S")
+                    # add to dictionary
+                    if date in usage_dict:
+                        usage_dict[date].append({'time': time, 'value': row[1]})
+                    else:
+                        usage_dict[date] = [{'time': time, 'value': row[1]}]
     # return dictionary formatted as a string
     if not usage_dict:
         return str("")
